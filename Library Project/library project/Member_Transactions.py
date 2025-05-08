ba5b301f0a0a8c82fc9e4ac18e782e9c57e1a823
icubuk <ibrahimcubuk@hotmail.com>
2025-05-08 14:37:49 +0200

    Update Member_Transactions.py

--- Library Project/library project/Member_Transactions.py
+++ Library Project/library project/Member_Transactions.py
@@ -1,4 +1,16 @@
-members=[]
+import json
+import os
+
+members_file = "members.json"
+if os.path.exists(members_file):
+    with open(members_file, "r", encoding="utf-8") as f:
+        members = json.load(f)
+else:
+    members = []
+
+def save_members():
+    with open(members_file, "w", encoding="utf-8") as f:
+        json.dump(members, f, ensure_ascii=False, indent=4)
 
 def members_print(members):
     for i in members:
@@ -20,4 +32,5 @@ def member_add(x):
             "adress" : member_adress}
     members.append(member_new)
     members_print(members)
+    save_members()
     print("------------Member added----------")
